e6b7d7a57c8a38c297e64e3ec522410d8d9e1919
root <root@localhost.localdomain>
2018-03-16 10:02:30 +0700

    Add kernel source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Fri Mar 16 02:48:53 +07 2018"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "localhost.localdomain"
+#define LINUX_COMPILER "gcc version 4.8.5 20150623 (Red Hat 4.8.5-16) (GCC)"
